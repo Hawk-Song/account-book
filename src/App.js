1c25f01678dd9798023e16ba74c271d080ad2599
Hawk Song <savokisong@gmail.com>
2020-06-02 23:40:53 -0700

    tab component and pricelist component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PriceList from './components/PriceList'
+import ViewTab from './components/ViewTab';
+import {LIST_VIEW, CHAT_VIEW} from "./utility"
+
+const items = [
+  {
+    "id": 1,
+    "title": "Travel to SF",
+    "price": 200,
+    "date": "2019-09-10",
+    "category": {
+      "id": "1",
+      "name": "travel",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  },
+  {
+    "id": 2,
+    "title": "Travel to SF",
+    "price": 400,
+    "date": "2019-09-10",
+    "category": {
+      "id": "1",
+      "name": "travel",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  },
+  {
+    "id": 3,
+    "title": "Travel to SF",
+    "price": 600,
+    "date": "2019-09-10",
+    "category": {
+      "id": "1",
+      "name": "travel",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ViewTab 
+        activeTab={LIST_VIEW}
+        onTabChange={(view) => {console.log(view)}}
+      />
+      <PriceList 
+        items={items} 
+        onModifyItem={(item) => {alert(item.id)}}
+        onDeleteItem={(item) => {alert(item.id)}}
+      />
     </div>
   );
 }
---
+++ src/utility.js
@@ -0,0 +1,2 @@
+export const LIST_VIEW = 'list'
+export const CHAT_VIEW = 'chart'
